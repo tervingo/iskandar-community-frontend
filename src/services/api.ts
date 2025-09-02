fdaea162a164a7cb6c4945058870604df10cc669
tervingo <keltivas@gmail.com>
2025-09-02 18:02:34 +0200

    Close session when browser (tab) is closed

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,7 +12,7 @@ const api = axios.create({
 
 // Add token to requests
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('auth_token');
+  const token = sessionStorage.getItem('auth_token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
