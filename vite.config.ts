6dde80f67dc4a58d95356ec420ec6cb9c604a70e
tervingo <keltivas@gmail.com>
2025-09-21 17:48:44 +0200

    proxy problems

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://iskandar-community-backend.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
 })
